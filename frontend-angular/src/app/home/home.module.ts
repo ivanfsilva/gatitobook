eec8aae5d3fbbca00d66313889a01cb660376bfa
ivanfsilva <ivanfs27@gmail.com>
2021-12-05 10:44:57 -0300

    Reactive Form

--- frontend-angular/src/app/home/home.module.ts
+++ frontend-angular/src/app/home/home.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { MensagemModule } from "../componentes/mensagem/mensagem.module";
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
-
 
 @NgModule({
   declarations: [
@@ -22,6 +22,7 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
 
     MensagemModule
   ]
