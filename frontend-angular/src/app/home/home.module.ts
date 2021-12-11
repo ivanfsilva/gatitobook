2061bf9cf29b09b744f2898a7144d560692aa2e4
ivanfsilva <ivanfs27@gmail.com>
2021-12-11 12:48:05 -0300

    Organization of modules

--- frontend-angular/src/app/home/home.module.ts
+++ frontend-angular/src/app/home/home.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
-import { MensagemModule } from "../componentes/mensagem/mensagem.module";
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -22,9 +21,7 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
-
-    MensagemModule
+    SharedModule,
   ]
 })
 export class HomeModule { }
